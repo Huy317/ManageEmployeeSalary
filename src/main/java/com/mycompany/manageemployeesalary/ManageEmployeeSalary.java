d57555d73b94d6a93fb370cfd8066805b29ac304
Huy317 <167590855+Huy317@users.noreply.github.com>
2024-05-04 09:26:42 +0700

    Initial commit

---
+++ src/main/java/com/mycompany/manageemployeesalary/ManageEmployeeSalary.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.manageemployeesalary;
+
+/**
+ *
+ * @author Student
+ */
+public class ManageEmployeeSalary {
+
+    public static void main(String[] args) {
+        
+    }
+}
